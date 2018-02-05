c1529058ae46844588e27e76d2e282c105cf013c
Henry <kevincch1999@gmail.com>
2018-02-05 11:39:36 +0800

    Initial commit

---
+++ root/FYP_php/admin/function.php
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Function</title>
+  <link rel="stylesheet" href="../css/index.css">
+</head>
+    <body>
+
+
+
+            <div class="centerBody">
+            <div class="header">
+            <?php include 'logout.html'; ?>
+             </div>
+             <?php include 'Menu.html'; ?>
+
+             <h1>function</h1>
+             <ul>
+               <li><a href=""><h2>upload map</h2></a></li>
+               <li><a href=""><h2>set car park</h2></a></li>
+               <li><a href=""><h2>user information</h2></a></li>
+             </ul>
+
+    </body>
+</html>
---
+++ root/FYP_php/member/Option.php
@@ -0,0 +1,22 @@
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Option</title>
+  <link rel="stylesheet" href="../css/index.css">
+</head>
+    <body>
+
+
+
+            <div class="centerBody">
+            <div class="header">
+            <?php include 'logout.html'; ?>
+             </div>
+             <?php include 'Menu.html'; ?>
+
+             <h1>Option</h1>
+
+
+    </body>
+</html>
